c494e32726a1a2e46d91e581c683e471ae7f0246
Syrone <cazonove@gmail.com>
2023-02-19 11:22:01 +0300

    Added Slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,25 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
-import mixitup from 'mixitup';
+import mixitup from 'mixitup'
+import rate from 'rateyo'
+import slick from 'slick-carousel'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	var mixer = mixitup('.products__inner-box');
+		var mixer = mixitup('.products__inner-box')
+
+		$(function() {
+			$('.rate-star').rateYo({
+				rating: 5,
+				readOnly: true,
+				starWidth: "13px"
+			})
+		})
+		$('.product-slider__inner').slick({
+			slidesToShow: 4,
+			slidesToScroll: 2,
+			dots: true,
+			arrows: false
+		})
 
 })
